8d0610c0e9ecef3c7a4cdfe992946f53475e233a
Usuario <Usuario@CPD02-M13>
2024-04-30 10:56:03 -0300

    Adicionar arquivos de projeto.

---
+++ MesesArray/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MesesArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] meses = { "Esse mês não existe. Digite outro número.", "Janeiro", "fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro" };
+
+            Console.WriteLine("Digite o número do mês que você escolheu: ");
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("O mês escolhido foi: "+ meses[n]);
+
+            Console.ReadKey();
+        }
+    }
+}
